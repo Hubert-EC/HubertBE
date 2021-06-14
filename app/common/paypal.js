0e35fab625ba1fd7a6d3cce5c7ee6f593ab91d1c
theleehari <nhai30928@gmail.com>
2021-06-14 16:16:39 +0700

    delete app

--- app/common/paypal.js
+++
@@ -1,8 +0,0 @@
-const paypal = require("paypal-rest-sdk");
-const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET } = require("../common/config");
-
-paypal.configure({
-  mode: "sandbox",
-  client_id: PAYPAL_CLIENT_ID,
-  client_secret: PAYPAL_CLIENT_SECRET,
-});
--- app/router/index.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.use("/auth", require("./auth.router"));
-router.use("/transport", require("./transport.router"));
-router.use("/paypal", require("./paypal.router"));
-module.exports = router;
-
