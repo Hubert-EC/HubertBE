82b62b87f1e45d0cf74928f1ba76434452d720eb
theleehari <nhai30928@gmail.com>
2021-06-19 14:05:52 +0700

    main

--- app/router/transport.router.js
+++
@@ -1,8 +0,0 @@
-const controller = require("../controllers/Transport.Controller.js");
-const { verifyToken } = require("../services/Token.Services");
-const express = require("express");
-const router = express.Router();
-
-router.route('/calculate-ship-price').post(verifyToken, controller.handleCalculateShipPrice)
-
-module.exports = router;
